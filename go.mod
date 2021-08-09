c046d4d2fed2ab04bceabfd0066b3328be42f387
Nam Huynh <namhq.1989@gmail.com>
2021-08-09 10:20:27 +0700

    init module

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/Selly-Modules/mongodb
+
+go 1.16
+
+require (
+	github.com/logrusorgru/aurora v2.0.3+incompatible
+	go.mongodb.org/mongo-driver v1.7.1
+)
---
+++ index.go
@@ -0,0 +1,48 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/x/bsonx"
+)
+
+// CreateIndex ...
+func CreateIndex(colName string, ind mongo.IndexModel) {
+	// Get collection
+	col := db.Collection(colName)
+	if col == nil {
+		fmt.Printf("Collection %s not existed", colName)
+		return
+	}
+
+	opts := options.CreateIndexes().SetMaxTime(time.Minute * 10)
+	_, err := col.Indexes().CreateOne(context.Background(), ind, opts)
+	if err != nil {
+		fmt.Printf("Index collection %s err: %v", col.Name(), err)
+	}
+}
+
+// NewIndexKey ...
+func NewIndexKey(key ...string) mongo.IndexModel {
+	var doc bsonx.Doc
+	for _, s := range key {
+		e := bsonx.Elem{
+			Key:   s,
+			Value: bsonx.Int32(1),
+		}
+		if strings.HasPrefix(s, "-") {
+			e = bsonx.Elem{
+				Key:   strings.Replace(s, "-", "", 1),
+				Value: bsonx.Int32(-1),
+			}
+		}
+		doc = append(doc, e)
+	}
+
+	return mongo.IndexModel{Keys: doc}
+}
